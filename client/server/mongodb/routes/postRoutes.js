681f2e18bb5a29f8ce2cd03b46b2cda0334a0f6a
EightFiveZeroEight <Business.8508@gmail.com>
2023-01-25 20:53:21 -0500

    Check Index for bugs

--- client/server/mongodb/routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Express } from "express";
-import * as dotenv from "dotenv";
-import {v2 as coudinary} from 'cloudinary'
-import Post from '../mongodb/models/post.js'
-
-dotenv.config()
-
-const router =express.Router();
